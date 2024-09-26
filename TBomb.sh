7006fa6e6e15bb494efcdbb54d197b1a91929197
TheZoneOfSecurity <xerohero220@gmail.com>
2024-09-26 22:11:03 +0530

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -38,8 +38,7 @@ banner() {
         toilet -f mono12 -F border SpeedX
     fi
     echo -e "\e[1;34m For Any Queries Join Me!!!\e[0m"
-    echo -e "\e[1;32m           Telegram: https://t.me/TBombChat \e[0m"
-    echo -e "\e[4;32m   YouTube: https://www.youtube.com/c/SpeedXTech \e[0m"
+    echo -e "\e[1;32m         Presented  Telegram: https://t.me/thezoneofsecurity \e[0m"
     echo " "
     echo "NOTE: Kindly move to the PIP version Of TBomb for more stability."
     echo " "
